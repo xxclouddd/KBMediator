7bce4ffc37563e881095adf7ab4cc3e97baeebac
xiaoxiong <xiaoxiong@meitunmama.com>
2019-06-24 16:17:35 +0800

    hello

--- KBMediator/AppDelegate.m
+++ KBMediator/AppDelegate.m
@@ -3,7 +3,7 @@
 //  KBMediator
 //
 //  Created by 肖雄 on 17/3/28.
-//  Copyright © 2017年 kuaibao. All rights reserved.
+//  Copyright © 2017年 xiaoxiong. All rights reserved.
 //
 
 #import "AppDelegate.h"
